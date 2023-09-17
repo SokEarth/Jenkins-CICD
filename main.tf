d74bd146f5bb2a3314109e170e9764dd73d6ce03
So Ezeh <117824198+SokEarth@users.noreply.github.com>
2023-09-17 03:20:07 +0100

    FIX: EIP #one' network interface

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_network_interface" "web-server-nic" {
 
 resource "aws_eip" "one" {
   domain                    = "vpc"
-  network_interface         = aws_network_interface.web-server-nic.id
+  network_interface         = aws_network_interface.web-server-nic
   associate_with_private_ip = "10.0.1.50"
   depends_on = [ aws_internet_gateway.gw ]
 }
